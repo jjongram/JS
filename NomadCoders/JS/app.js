e8763ffb93c6b02892a1e157816ad0b5bf831cc4
jjongram <73387968+jjongram@users.noreply.github.com>
2022-06-30 21:52:56 +0900

    hi

---
+++ NomadCoders/JS/app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = 'Jongin';
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('Hello ' + myName);
+
+myName = 'Jong';
+
+console.log('your new name is ' + myName);
