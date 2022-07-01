d3f55e54e7cca77ca816556cf97bccc932c4f9c5
jjongram <73387968+jjongram@users.noreply.github.com>
2022-07-01 23:32:33 +0900

    asd

--- NomadCoders/JS/app.js
+++ NomadCoders/JS/app.js
@@ -1,12 +1,2 @@
-const a = 5;
-const b = 2;
-let myName = 'Jongin';
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log('Hello ' + myName);
-
-myName = 'Jong';
-
-console.log('your new name is ' + myName);
+const daysOfWeek = ['mon', 'tue', 'wed', 'fri', 'sat', 'sun'];
+console.log(daysOfWeek[4]);
