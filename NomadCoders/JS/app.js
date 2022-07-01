f22c06a975a71e3d60e1e3b19819434bb17a5383
jjongram <73387968+jjongram@users.noreply.github.com>
2022-07-01 23:35:53 +0900

    asd

--- NomadCoders/JS/app.js
+++ NomadCoders/JS/app.js
@@ -1,2 +1,9 @@
-const daysOfWeek = ['mon', 'tue', 'wed', 'fri', 'sat', 'sun'];
+const daysOfWeek = ['mon', 'tue', 'wed', 'fri', 'sat'];
+
+// Get Item from Array
 console.log(daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push('sun');
+
+console.log(daysOfWeek);
