5e279624ba8120be783d9a0e70aa45d1dd19af7f
jjongram <73387968+jjongram@users.noreply.github.com>
2022-07-04 23:47:29 +0900

    asd

--- NomadCoders/JS/app.js
+++ NomadCoders/JS/app.js
@@ -1,2 +1,16 @@
-const toBuy = ['potato', 'tomato', 'pizza'];
-console.log(toBuy[398409342]);
+const playerName = 'jongin';
+const playerPoints = 121212;
+const playerHandsome = true;
+const playerFat = 'no';
+
+player.name;
+player.points;
+player.handsome;
+
+const player = {
+  name: 'jogngin',
+  points: 10,
+  fat: true,
+};
+console.log(player);
+console.log(player.name);
