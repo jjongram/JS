a19997f954e18bd286b507d67912f9e452c5422b
jjongram <73387968+jjongram@users.noreply.github.com>
2022-07-04 23:56:26 +0900

    ㅁㄴㅇ

--- NomadCoders/JS/app.js
+++ NomadCoders/JS/app.js
@@ -1,12 +1,3 @@
-const playerName = 'jongin';
-const playerPoints = 121212;
-const playerHandsome = true;
-const playerFat = 'no';
-
-player.name;
-player.points;
-player.handsome;
-
 const player = {
   name: 'jogngin',
   points: 10,
