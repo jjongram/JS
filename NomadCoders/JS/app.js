3a3d8a5aaf2cab79597312272370d5677d587f57
jjongram <73387968+jjongram@users.noreply.github.com>
2022-07-05 21:18:40 +0900

    chore

--- NomadCoders/JS/app.js
+++ NomadCoders/JS/app.js
@@ -3,17 +3,17 @@ const calculator = {
     console.log(a + b);
   },
   min: function (a, b) {
-    console.log(a - b)
+    console.log(a - b);
   },
   div: function (a, b) {
-    console.log(a / b)
-  }
-  powerof: function(a, b) {
-    console.log(a**b)
-  }
+    console.log(a / b);
+  },
+  powerof: function (a, b) {
+    console.log(a ** b);
+  },
 };
 
-calculator.add(5, 2)
-calculator.min(5,2)
-calculator.div(5, 2)
-calculator.powerof(5, 2)
+calculator.add(5, 2);
+calculator.min(5, 2);
+calculator.div(5, 2);
+calculator.powerof(5, 2);
