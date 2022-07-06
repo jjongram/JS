9b3d4100df5f7fd6fd070b12085b0772485db8ef
jjongram <73387968+jjongram@users.noreply.github.com>
2022-07-07 04:06:30 +0900

    chore

--- NomadCoders/JS/app.js
+++ NomadCoders/JS/app.js
@@ -1,13 +1,13 @@
-Constant age = parseInt(prompt("How old are you?"))
+const age = parseInt(prompt('How old are you?'));
 
 if (isNaN(age)) {
-    console.log("Please write a real positive number")
+  console.log('Please write a real positive number');
 } else if (age < 18) {
-    console.log("You are too young")
+  console.log('You are too young');
 } else if (age >= 18 && age <= 50) {
-    console.lob("You can drink")
+  console.lob('You can drink');
 } else if (age > 50 && age <= 80) {
-    console.log("You should exercise")
+  console.log('You should exercise');
 } else if (age > 80) {
-    console.log("You can do whatever you want")
+  console.log('You can do whatever you want');
 }
